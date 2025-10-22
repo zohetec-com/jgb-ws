0724214654fc3c96dafd7c77c982022e3ac57040
黄志敏 <huangzhimin@zohetec.com>
2025-10-22 15:29:49 +0800

    up

--- client/main.cpp
+++ client/main.cpp
@@ -3,9 +3,11 @@
 #include <jgb/log.h>
 #include <jgb/core.h>
 #include <jgb/helper.h>
+#include <getopt.h>
 
 extern jgb_api_t wsapp;
 extern jgb_api_t ws_client;
+extern int jgb_print_level;
 
 static bool exit_flag = false;
 
@@ -49,7 +51,7 @@ static void stop(void)
     }
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     // 注册 SIGINT 信号处理函数
     struct sigaction act = {};
@@ -65,6 +67,43 @@ int main()
         perror("sigaction");
     }
 
+    const char* protocol = "ws-sync";
+    const char* server = "127.0.0.1";
+    const char* port = "8000";
+
+    static struct option long_options[] = {
+        {"protocol", required_argument, 0, 0},
+        {0, 0, 0, 0} // Sentinel to mark the end of the array
+    };
+
+    int long_index = 0; // To store the index of the long option found
+    int opt;
+
+    while ((opt = getopt_long(argc, argv, "h:p:v:", long_options, &long_index)) != -1) {
+        switch (opt) {
+        case 0: // This case is for long options that don't have a short option equivalent
+            jgb_debug("{ index = %d, opt = %s }", long_index, long_options[long_index].name);
+            if(long_index == 0) // protocol
+            {
+                protocol = optarg;
+            }
+            break;
+        case 'h':
+            server = optarg;
+            break;
+        case 'p':
+            port = optarg;
+            break;
+        case 'v':
+            jgb::stoi(optarg, jgb_print_level);
+            break;
+        default:
+            break;
+        }
+    }
+
+    std::string url = "ws://" + std::string(server) + ":" + std::string(port);
+
     jgb::core::get_instance()->install("wsapp", &wsapp);
     jgb::core::get_instance()->install("ws_client", &ws_client);
 
@@ -74,8 +113,8 @@ int main()
     r = jgb::core::get_instance()->root_conf()->get("ws_client", &conf);
     if(!r)
     {
-        conf->create("protocol", "ws-sync");
-        conf->create("url", "ws://127.0.0.1:8000");
+        conf->create("protocol", protocol);
+        conf->create("url", url);
     }
     else
     {
